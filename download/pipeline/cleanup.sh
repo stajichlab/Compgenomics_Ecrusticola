1fbf0b138ddd76738a46b5fbfaaee55ed6e568bc
G-gnome <kkell060@ucr.edu>
2024-09-23 09:09:47 -0700

    change assemblies names to sepcies names

--- download/pipeline/cleanup.sh
+++ download/pipeline/cleanup.sh
@@ -2,3 +2,12 @@
 
 rm -rf ncbi_dataset.zip
 rm -rf ncbi_dataset
+# remove assemblies without a species name
+rm assemblies/Exophiala_sp._BO6.fasta
+rm assemblies/Exophiala_sp..fasta
+rm assemblies/Exophiala_sp._HKRS030.fasta
+rm assemblies/Exophiala_sp._BO6.fasta
+rm assemblies/Exophiala_sp._BO6.fasta
+rm assemblies/Exophiala_sp._LBMH1013.fasta
+
+
