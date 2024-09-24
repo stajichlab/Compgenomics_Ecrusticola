ba50af87a5b3c12200565de67b5ddcf549c6aa91
G-gnome <kkell060@ucr.edu>
2024-09-24 09:51:29 -0700

    Add make_sampfile.py

---
+++ annotate/scripts/make_sampfile.py
@@ -0,0 +1,25 @@
+# %% [markdown]
+# #### Import libraries
+
+# %%
+import pandas as pd
+
+# %%
+meta = pd.read_csv('filtered_metadata.tsv', sep='\t')
+
+# %%
+meta['PHYLUM'] = 'Ascomycota'
+
+# %%
+column_list = meta.columns.tolist()
+column_list.remove('Organism Name')
+column_list.remove('PHYLUM')
+
+# %%
+samples = meta.drop(columns=column_list)
+samples.columns = samples.columns.str.replace(' ', '_')
+
+# %%
+samples.to_csv('samples.csv', index=False)
+
+
