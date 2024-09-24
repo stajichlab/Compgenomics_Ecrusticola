0a9798a98057d25714547306942fa980cd9b58be
G-gnome <kkell060@ucr.edu>
2024-09-24 10:53:03 -0700

    remove whitespace from samples.csv in py script

--- annotate/scripts/make_sampfile.py
+++ annotate/scripts/make_sampfile.py
@@ -1,25 +1,40 @@
 # %% [markdown]
-# #### Import libraries
+#  #### Import libraries
 
+# %%
 # %%
 import pandas as pd
 
+
+# %%
 # %%
 meta = pd.read_csv('filtered_metadata.tsv', sep='\t')
 
+
+# %%
 # %%
 meta['PHYLUM'] = 'Ascomycota'
 
+
+# %%
 # %%
 column_list = meta.columns.tolist()
 column_list.remove('Organism Name')
 column_list.remove('PHYLUM')
 
+
+# %%
 # %%
 samples = meta.drop(columns=column_list)
 samples.columns = samples.columns.str.replace(' ', '_')
+samples['Organism_Name'] = samples['Organism_Name'].str.replace(' ', '_')
+
 
 # %%
+# %%
 samples.to_csv('samples.csv', index=False)
+
+
+
 
 
