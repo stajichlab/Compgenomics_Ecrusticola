7df89819a34903354189e0c16423334a7723a7e0
G-gnome <kkell060@ucr.edu>
2024-10-08 14:01:55 -0700

    Add indexing to contig headers to get only unique strings

--- annotate/rename_header_fasta.py
+++ annotate/rename_header_fasta.py
@@ -10,7 +10,7 @@ import os
 path = "./genomes/"
 # obtain genome file names that are masked for repeats
 for file in os.listdir(path):
-    if file.endswith("_filtered.fasta"):
+    if file.endswith("fasta.masked"):
         print(file)
         # Read in each of the files
         with open(os.path.join(path, file), "r") as f_in:
@@ -22,7 +22,9 @@ for file in os.listdir(path):
         # Reformat and write each sequence to a new file
         new_file_name = os.path.join(path, f"reformat_{file}")
         with open(new_file_name, "w") as f_out:
+            i = 0
             for sequence in sequences:
+                i += 1
                 # Split the header from the sequence by the first newline
                 header, seq = sequence.split("\n", 1)
                 # Remove any internal newlines in the sequence and ensure sequence is on a single line
@@ -32,8 +34,8 @@ for file in os.listdir(path):
                 new_header = f">{contig}"
                 # Remove any zeros from the contig name
                 new_header = new_header.replace("0", "")
-                # Write the new header and sequence to the file
-                f_out.write(new_header + "\n")
+                # Write the new header and index and sequence to the file
+                f_out.write(new_header + str(i) + "\n")
                 f_out.write(seq + "\n")
 
 
