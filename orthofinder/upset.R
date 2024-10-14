bb79090b5487bb6df6553ca2242c05dc96ade076
G-gnome <kkell060@ucr.edu>
2024-10-14 15:54:41 -0700

    fix exclusion of largest set

--- orthofinder/upset.R
+++ orthofinder/upset.R
@@ -1,26 +1,30 @@
-# Load the UpSetR library
 library("UpSetR")
 
 # Load the data
 orthogroups_df <- read.table("Orthogroups.GeneCount.tsv", header = TRUE, stringsAsFactors = FALSE)
 
-# Select species columns, excluding the first one
-selected_species <- colnames(orthogroups_df)[3:(ncol(orthogroups_df) - 1)]
+# Select species columns
+selected_species <- colnames(orthogroups_df)[2:(ncol(orthogroups_df) - 1)]
+selected_species
 
 # Convert counts to binary presence/absence
 orthogroups_df[orthogroups_df > 0] <- 1
 
-# Remove the rows where all species have a value of 1 (i.e., shared by all species)
-orthogroups_df <- orthogroups_df[rowSums(orthogroups_df[selected_species]) != length(selected_species), ]
+# Print the first few rows of the dataframe for preview
+print(head(orthogroups_df, n = 20))
 
-# Save the UpSet plot as PNG
-png(file = "upset_plot.png", width = 1200, height = 800)  # Increase dimensions
+# Filter out the intersection where all selected species are present
+filtered_data <- orthogroups_df[!(orthogroups_df$Exophiala_crusticola == 1 & 
+                                  orthogroups_df$Exophiala_xenobiotica == 1 & 
+                                  orthogroups_df$Exophiala_spinifera == 1 & 
+                                  orthogroups_df$Exophiala_viscosa == 1 & 
+                                  orthogroups_df$Exophiala_sideris == 1 & 
+                                  orthogroups_df$Exophiala_mesophila == 1 & 
+                                  orthogroups_df$Exophiala_lecanii.corni == 1 & 
+                                  orthogroups_df$Exophiala_alcalophila == 1), ]
 
-# Create the UpSet plot without the shared intersection
-upset(orthogroups_df, 
-      nsets = length(selected_species), 
-      sets = rev(selected_species), 
-      keep.order = TRUE, 
-      order.by = "freq")
-
+# Save the UpSet plot as PNG with increased dimensions
+png(file = "upset_plot.png", width = 1200, height = 800)
+upset(filtered_data, nsets = ncol(filtered_data), sets = rev(selected_species), 
+      keep.order = TRUE, order.by = "freq")
 dev.off()  # Close the PNG device
