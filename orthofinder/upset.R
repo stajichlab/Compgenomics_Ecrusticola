f0cba127ffb3cce9e49b6894e0a72147176508db
G-gnome <kkell060@ucr.edu>
2024-10-14 16:10:23 -0700

    update upset plot with core genome label

--- orthofinder/upset.R
+++ orthofinder/upset.R
@@ -1,4 +1,5 @@
 library("UpSetR")
+library("grid")
 
 # Load the data
 orthogroups_df <- read.table("Orthogroups.GeneCount.tsv", header = TRUE, stringsAsFactors = FALSE)
@@ -13,18 +14,17 @@ orthogroups_df[orthogroups_df > 0] <- 1
 # Print the first few rows of the dataframe for preview
 print(head(orthogroups_df, n = 20))
 
-# Filter out the intersection where all selected species are present
-filtered_data <- orthogroups_df[!(orthogroups_df$Exophiala_crusticola == 1 & 
-                                  orthogroups_df$Exophiala_xenobiotica == 1 & 
-                                  orthogroups_df$Exophiala_spinifera == 1 & 
-                                  orthogroups_df$Exophiala_viscosa == 1 & 
-                                  orthogroups_df$Exophiala_sideris == 1 & 
-                                  orthogroups_df$Exophiala_mesophila == 1 & 
-                                  orthogroups_df$Exophiala_lecanii.corni == 1 & 
-                                  orthogroups_df$Exophiala_alcalophila == 1), ]
+# Create a custom color vector for the intersections
+# Blue for intersections containing Exophiala_crusticola, red otherwise
+set_colors <- ifelse(orthogroups_df$Exophiala_crusticola == 1, "blue", "red")
 
 # Save the UpSet plot as PNG with increased dimensions
 png(file = "upset_plot.png", width = 1200, height = 800)
-upset(filtered_data, nsets = ncol(filtered_data), sets = rev(selected_species), 
-      keep.order = TRUE, order.by = "freq")
+upset(orthogroups_df, nsets = ncol(orthogroups_df), sets = rev(selected_species), 
+      keep.order = TRUE, order.by = "freq", 
+      main.bar.color = set_colors)  # Set the colors for the intersections
+
+# Add custom text to the middle of the plot using grid.text
+grid.text("Core genome = 3610", x = .62, y = .7, gp = gpar(fontsize = 15, col = "black"))
+
 dev.off()  # Close the PNG device
